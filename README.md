be2385881d8271210ba95927d63ebc0e9c86e05a
WyattAlexander-001 <wyattbushman117@gmail.com>
2024-10-30 01:58:30 -0400

    finally revisited ruby and doing exercises

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# OOP-With-Ruby
-Practice examples of Ruby using OOP paradigm
+# OOP With Ruby
+Practice examples of Ruby using OOP paradigm using Odin Project
+
+
+## Notes:
+* Rube is STRONGLY OOP Language == Everything is an object, even basic data types
+
+
---
+++ intro.rb
@@ -0,0 +1,38 @@
+puts 6.even? # => true
+puts 7.even? # => false
+puts 6.odd? # => false
+
+puts 13.to_f # => 13.0
+puts 47.8.to_i # => 47
+
+puts "This is concat".concat("enation")
+puts "This is concat" + "enation"
+puts "This is concat" << "enation"
+
+puts "Wyatt"[0] # => W
+puts "Wyatt"[0,3] # => Wya So it's 0 up to but not including 3 So 0 to blah-1
+puts "Wyatt"[0..3] # => Wyat So it's 0 up to and including 3 So 0 to blah
+puts "Wyatt"[-1] # => t So it's the last character
+
+name = "Jimmy McGill" 
+
+puts "My name is, #{name}, I'm a lawyer, so Better Call Saul!"
+
+puts "exampleofusingupcase".upcase
+puts "EXAMPLEOFUSINGDOWNCASE".downcase
+puts "exampleofusingcapitalize".capitalize
+
+puts "I am a sentence".length # => 15 as it counts spaces
+
+puts "Wyatt".sub("t", "o") # => Wyaot replaces first instance of t with o
+puts "Wyatt".gsub("t", "o") # => Wyaoo replaces all instances of t with o
+
+puts "My favorite game is:".insert(-1, "Pikmin") # => My favorite game is:Pikmin
+puts "Cruelty Squad is about being cruel like it's in the name and it isn't crap man!".delete("crap")
+
+puts 5.to_s
+puts nil.to_s
+puts :symbol.to_s # Symbols are stored in memory once, strings are stored in memory every time they are used
+
+# :wyatt_symbol = "Wyatt" # => SyntaxError: (irb):1: syntax error, unexpected '=', expecting end-of-input
+
